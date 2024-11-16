fbe6f8937b1a2ddfaabf01bf881074ecc3dd3f86
deleu <deleuzethomasbisson@gmail.com>
2024-11-15 20:51:20 -0500

    colors

---
+++ create_colors.py
@@ -0,0 +1,36 @@
+import random
+
+def get_the_colors(len,type):
+    if type == "O":
+        return [
+    len * [173, 216, 230],
+
+]
+    elif type == "B":
+        list_b = [[194, 106, 54],[204, 85, 0],[139, 69, 19],[141, 85, 36]]
+        return  [
+    len * random.choice[list_b]
+]
+    elif type == "A":
+        list_a = [[255, 255, 102],[250, 250, 210],[238, 232, 170],[255, 204, 51]]
+    elif type == "F":
+    elif type == "G":
+    elif type == "K":
+    elif type == "M":
+    elif type == "Brown Dwarf":
+    elif type == "SE":
+    elif type == "MN":
+    elif type == "N":
+    elif type == "J":
+    elif type == "HJ":
+    elif type == "USP":
+    elif type == "ESP":
+    elif type == "MP":
+    elif type == "PP":
+    elif type == "SN":
+    elif type == "death_star":
+    elif type == "tattoine":
+    elif type == "corruscant":
+    elif type == "gargantua":
+
+
